77febd95f1bad6545a11763b3c0d9fb265746ba5
Aleksandr Parshukov <porshe97@yandex.ru>
2020-10-21 10:13:16 +0300

    Оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -1,26 +1,74 @@
-const bookingButton = document.querySelector(" .booking-button ");
-const modal = document.querySelector(" .modal ");
-const guests = modal.querySelectorAll(" .modal-columns ");
+const MIN_KIDS_VALUE = 0;
+const MAX_KIDS_VALUE = 20;
+const MIN_ADULTS_VALUE = 1;
+const MAX_ADULTS_VALUE = 20;
+
+const bookingButton = document.querySelector(".booking-button");
+const modal = document.querySelector(".modal ");
+const guests = modal.querySelectorAll(".modal-columns");
+const dateArrival = modal.querySelector("#modal-arrival-date");
+const dateDeparture = modal.querySelector("#modal-departure-date");
+const adultsValue = guests[0].querySelector(".modal-input-number");
+const kidsValue = guests[1].querySelector(".modal-input-number");
+const modalSubmit = modal.querySelector(".button-link");
+
+let minusBtn = guests[0].querySelector(".modal-minus");
+let plusBtn = guests[0].querySelector(".modal-plus");
+
+
+/* Modal */
+modal.classList.add("modal-hidden");
 
 bookingButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modal.classList.toggle("modal-visible");
+  modal.classList.toggle("modal-hidden");
 });
 
-for (let i = 0; i < guests.length; i++) {
-  const minusBtn = guests[i].querySelector(" .modal-minus ");
-  const plusBtn = guests[i].querySelector(" .modal-plus ");
-  let input = guests[i].querySelector(" .modal-input-number ");
 
-  minusBtn.addEventListener("click", function () {
-    if (input.value > 0) {
-      input.value = String(+input.value - 1);
-    }
-  });
-  plusBtn.addEventListener("click", function () {
-    if (input.value < 10) {
-      input.value = String(+input.value + 1);
-    }
-  });
+/* Modal Inputs */
+minusBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (adultsValue.value > MIN_ADULTS_VALUE) {
+    adultsValue.value = String(+adultsValue.value - 1);
+  }
+});
+
+plusBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (adultsValue.value < MAX_ADULTS_VALUE) {
+    adultsValue.value = String(+adultsValue.value + 1);
+  }
+});
+
+minusBtn = guests[1].querySelector(".modal-minus");
+plusBtn = guests[1].querySelector(".modal-plus");
+
+minusBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (kidsValue.value > MIN_KIDS_VALUE) {
+    kidsValue.value = String(+kidsValue.value - 1);
+  }
+});
+plusBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (kidsValue.value < MAX_KIDS_VALUE) {
+    kidsValue.value = String(+kidsValue.value + 1);
+  }
+});
+
+
+/* Validation */
+modalSubmit.addEventListener("click", function (evt){
+  evt.preventDefault();
+  checkInput(dateArrival);
+  checkInput(dateDeparture);
+});
+
+function checkInput (input) {
+  if (!input.value) {
+    input.classList.add("input-error");
+    return false;
+  }
+  input.classList.remove("input-error");
+  return true;
 }
-
